1768f2ba5001a659b97577b889c254c3747cceab
luoxiao <2362844711@qq.com>
2023-06-06 19:36:45 +0800

    Add copy-test

--- projects/mmdet3d_plugin/uniad/modules/transformer.py
+++ projects/mmdet3d_plugin/uniad/modules/transformer.py
@@ -111,86 +111,97 @@ class PerceptionTransformer(BaseModule):
         """
         obtain bev features.
         """
+        self.___id___ = 0
+        def get_id():
+            self.___id___ += 1
+            return self.___id___
+
         with torch.profiler.record_function("PerceptionTransformer(Detr3D)-get_bev_features"):
-            bs = mlvl_feats[0].size(0)
-            bev_queries = bev_queries.unsqueeze(1).repeat(1, bs, 1)
-            bev_pos = bev_pos.flatten(2).permute(2, 0, 1)
-            # obtain rotation angle and shift with ego motion
-            delta_x = np.array([each['can_bus'][0]
-                            for each in img_metas])
-            delta_y = np.array([each['can_bus'][1]
-                            for each in img_metas])
-            ego_angle = np.array(
-                [each['can_bus'][-2] / np.pi * 180 for each in img_metas])
-            grid_length_y = grid_length[0]
-            grid_length_x = grid_length[1]
-            translation_length = np.sqrt(delta_x ** 2 + delta_y ** 2)
-            translation_angle = np.arctan2(delta_y, delta_x) / np.pi * 180
-            bev_angle = ego_angle - translation_angle
-            shift_y = translation_length * \
-                np.cos(bev_angle / 180 * np.pi) / grid_length_y / bev_h
-            shift_x = translation_length * \
-                np.sin(bev_angle / 180 * np.pi) / grid_length_x / bev_w
-            shift_y = shift_y * self.use_shift
-            shift_x = shift_x * self.use_shift
-            shift = bev_queries.new_tensor(
-                [shift_x, shift_y]).permute(1, 0)  # xy, bs -> bs, xy
+            with torch.profiler.record_function("copy-test-".format(get_id())):
+                bs = mlvl_feats[0].size(0)
+                bev_queries = bev_queries.unsqueeze(1).repeat(1, bs, 1)
+                bev_pos = bev_pos.flatten(2).permute(2, 0, 1)
+            with torch.profiler.record_function("copy-test-".format(get_id())):
+                # obtain rotation angle and shift with ego motion
+                delta_x = np.array([each['can_bus'][0]
+                                for each in img_metas])
+                delta_y = np.array([each['can_bus'][1]
+                                for each in img_metas])
+                ego_angle = np.array(
+                    [each['can_bus'][-2] / np.pi * 180 for each in img_metas])
+                grid_length_y = grid_length[0]
+                grid_length_x = grid_length[1]
+                translation_length = np.sqrt(delta_x ** 2 + delta_y ** 2)
+                translation_angle = np.arctan2(delta_y, delta_x) / np.pi * 180
+                bev_angle = ego_angle - translation_angle
+                shift_y = translation_length * \
+                    np.cos(bev_angle / 180 * np.pi) / grid_length_y / bev_h
+                shift_x = translation_length * \
+                    np.sin(bev_angle / 180 * np.pi) / grid_length_x / bev_w
+                shift_y = shift_y * self.use_shift
+                shift_x = shift_x * self.use_shift
+                shift = bev_queries.new_tensor(
+                    [shift_x, shift_y]).permute(1, 0)  # xy, bs -> bs, xy
+            with torch.profiler.record_function("copy-test-".format(get_id())):
+                if prev_bev is not None:
+                    if prev_bev.shape[1] == bev_h * bev_w:
+                        prev_bev = prev_bev.permute(1, 0, 2)
+                    if self.rotate_prev_bev:
+                        for i in range(bs):
+                            rotation_angle = img_metas[i]['can_bus'][-1]
+                            tmp_prev_bev = prev_bev[:, i].reshape(
+                                bev_h, bev_w, -1).permute(2, 0, 1)
+                            tmp_prev_bev = rotate(tmp_prev_bev, rotation_angle,
+                                                center=self.rotate_center)
+                            tmp_prev_bev = tmp_prev_bev.permute(1, 2, 0).reshape(
+                                bev_h * bev_w, 1, -1)
+                            prev_bev[:, i] = tmp_prev_bev[:, 0]
 
-            if prev_bev is not None:
-                if prev_bev.shape[1] == bev_h * bev_w:
-                    prev_bev = prev_bev.permute(1, 0, 2)
-                if self.rotate_prev_bev:
-                    for i in range(bs):
-                        rotation_angle = img_metas[i]['can_bus'][-1]
-                        tmp_prev_bev = prev_bev[:, i].reshape(
-                            bev_h, bev_w, -1).permute(2, 0, 1)
-                        tmp_prev_bev = rotate(tmp_prev_bev, rotation_angle,
-                                            center=self.rotate_center)
-                        tmp_prev_bev = tmp_prev_bev.permute(1, 2, 0).reshape(
-                            bev_h * bev_w, 1, -1)
-                        prev_bev[:, i] = tmp_prev_bev[:, 0]
+            with torch.profiler.record_function("copy-test-".format(get_id())):
+                # add can bus signals
+                can_bus = bev_queries.new_tensor(
+                    [each['can_bus'] for each in img_metas])  # [:, :]
+                can_bus = self.can_bus_mlp(can_bus)[None, :, :]
+                bev_queries = bev_queries + can_bus * self.use_can_bus
 
-            # add can bus signals
-            can_bus = bev_queries.new_tensor(
-                [each['can_bus'] for each in img_metas])  # [:, :]
-            can_bus = self.can_bus_mlp(can_bus)[None, :, :]
-            bev_queries = bev_queries + can_bus * self.use_can_bus
+            with torch.profiler.record_function("copy-test-".format(get_id())):
+                feat_flatten = []
+                spatial_shapes = []
+                for lvl, feat in enumerate(mlvl_feats):
+                    bs, num_cam, c, h, w = feat.shape
+                    spatial_shape = (h, w)
+                    feat = feat.flatten(3).permute(1, 0, 3, 2)
+                    if self.use_cams_embeds:
+                        feat = feat + self.cams_embeds[:, None, None, :].to(feat.dtype)
+                    feat = feat + self.level_embeds[None,
+                                                    None, lvl:lvl + 1, :].to(feat.dtype)
+                    spatial_shapes.append(spatial_shape)
+                    feat_flatten.append(feat)
 
-            feat_flatten = []
-            spatial_shapes = []
-            for lvl, feat in enumerate(mlvl_feats):
-                bs, num_cam, c, h, w = feat.shape
-                spatial_shape = (h, w)
-                feat = feat.flatten(3).permute(1, 0, 3, 2)
-                if self.use_cams_embeds:
-                    feat = feat + self.cams_embeds[:, None, None, :].to(feat.dtype)
-                feat = feat + self.level_embeds[None,
-                                                None, lvl:lvl + 1, :].to(feat.dtype)
-                spatial_shapes.append(spatial_shape)
-                feat_flatten.append(feat)
+            with torch.profiler.record_function("copy-test-".format(get_id())):
+                feat_flatten = torch.cat(feat_flatten, 2)
+                spatial_shapes = torch.as_tensor(
+                    spatial_shapes, dtype=torch.long, device=bev_pos.device)
+                level_start_index = torch.cat((spatial_shapes.new_zeros(
+                    (1,)), spatial_shapes.prod(1).cumsum(0)[:-1]))
 
-            feat_flatten = torch.cat(feat_flatten, 2)
-            spatial_shapes = torch.as_tensor(
-                spatial_shapes, dtype=torch.long, device=bev_pos.device)
-            level_start_index = torch.cat((spatial_shapes.new_zeros(
-                (1,)), spatial_shapes.prod(1).cumsum(0)[:-1]))
+                feat_flatten = feat_flatten.permute(
+                    0, 2, 1, 3)  # (num_cam, H*W, bs, embed_dims)
 
-            feat_flatten = feat_flatten.permute(
-                0, 2, 1, 3)  # (num_cam, H*W, bs, embed_dims)
-
-            bev_embed = self.encoder(
-                bev_queries,
-                feat_flatten,
-                feat_flatten,
-                bev_h=bev_h,
-                bev_w=bev_w,
-                bev_pos=bev_pos,
-                spatial_shapes=spatial_shapes,
-                level_start_index=level_start_index,
-                prev_bev=prev_bev,
-                shift=shift,
-                img_metas=img_metas,
-            )
+            with torch.profiler.record_function("copy-test-".format(get_id())):
+                bev_embed = self.encoder(
+                    bev_queries,
+                    feat_flatten,
+                    feat_flatten,
+                    bev_h=bev_h,
+                    bev_w=bev_w,
+                    bev_pos=bev_pos,
+                    spatial_shapes=spatial_shapes,
+                    level_start_index=level_start_index,
+                    prev_bev=prev_bev,
+                    shift=shift,
+                    img_metas=img_metas,
+                )
 
             return bev_embed
     
