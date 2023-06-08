5a4c7447e2a9b3bf8ab0a268d14f8e641b8d2359
YTEP-ZHI <ytepjz@gmail.com>
2023-06-08 05:30:56 +0000

    enhance train/eval scripts: support multiple machine training

--- tools/uniad_dist_eval.sh
+++ tools/uniad_dist_eval.sh
@@ -8,9 +8,9 @@ CFG=$1                                               #
 CKPT=$2                                              #
 GPUS=$3                                              #    
 # -------------------------------------------------- #
+GPUS_PER_NODE=$(($GPUS<8?$GPUS:8))
 
-
-PORT=${PORT:-29503}
+MASTER_PORT=${MASTER_PORT:-28596}
 WORK_DIR=$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/
 # Intermediate files and logs will be saved to UniAD/projects/work_dirs/
 
@@ -19,7 +19,13 @@ if [ ! -d ${WORK_DIR}logs ]; then
 fi
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test.py $CFG $CKPT --launcher pytorch ${@:4} --eval bbox \
+python -m torch.distributed.launch \
+    --nproc_per_node=$GPUS_PER_NODE \
+    --master_port=$MASTER_PORT \
+    $(dirname "$0")/test.py \
+    $CFG \
+    $CKPT \
+    --launcher pytorch ${@:4} \
+    --eval bbox \
     --show-dir ${WORK_DIR} \
     2>&1 | tee ${WORK_DIR}logs/eval.$T
--- tools/uniad_dist_train.sh
+++ tools/uniad_dist_train.sh
@@ -7,9 +7,13 @@ T=`date +%m%d%H%M`
 CFG=$1                                               #
 GPUS=$2                                              #
 # -------------------------------------------------- #
+GPUS_PER_NODE=$(($GPUS<8?$GPUS:8))
+NNODES=`expr $GPUS / $GPUS_PER_NODE`
 
+MASTER_PORT=${MASTER_PORT:-28596}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+RANK=${RANK:-0}
 
-PORT=${PORT:-28599}
 WORK_DIR=$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/
 # Intermediate files and logs will be saved to UniAD/projects/work_dirs/
 
@@ -18,7 +22,15 @@ if [ ! -d ${WORK_DIR}logs ]; then
 fi
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CFG --launcher pytorch ${@:3} --deterministic \
+python -m torch.distributed.launch \
+    --nproc_per_node=${GPUS_PER_NODE} \
+    --master_addr=${MASTER_ADDR} \
+    --master_port=${MASTER_PORT} \
+    --nnodes=${NNODES} \
+    --node_rank=${RANK} \
+    $(dirname "$0")/train.py \
+    $CFG \
+    --launcher pytorch ${@:3} \
+    --deterministic \
     --work-dir ${WORK_DIR} \
     2>&1 | tee ${WORK_DIR}logs/train.$T
